7c99f4fcdafadf226148ed4185fd040410ebaca5
softkleenex <softkleenex1217@gmail.com>
2026-05-26 18:27:08 +0900

    gitignore에 KTC_code 추가 (교수님 레포지토리와 직접 연결)

--- KTC_code/week1-day1/wrap-up/07_ref.py
+++
@@ -1,9 +0,0 @@
-def parse_age(value):
-    try:
-        return int(value)
-    except ValueError:
-        print(f"'{value}'는 숫자가 아닙니다.")
-        return None
-
-print(parse_age("23"))      # 23
-print(parse_age("hello"))   # 오류 메시지 출력 후 None 반환 (프로그램 계속 실행)
